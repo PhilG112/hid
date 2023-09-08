5b1d06b97a2312b661f0f8557c8a0d8ca8c971bc
Philg112 <pgrujovski@protonmail.com>
2023-09-08 16:37:40 +1000

    let writer cook...

--- src/Ch5/GenSQL.hs
+++ src/Ch5/GenSQL.hs
@@ -5,14 +5,14 @@ module Ch5.GenSQL where
 
 import Control.Monad.Writer (MonadWriter (tell), Writer)
 import Data.Text (Text, splitOn)
+import qualified Data.Text as T
 
 type SQL = Text
 
 data ErrorMsg = WrongFormat Int Text
     deriving (Show)
 
-genSQL :: Text -> Writer [ErrorMsg] SQL
-genSQL _ = _
+genSQL txt = traverse processLine (zip [1 ..] $ T.lines txt)
 
 genInsert :: Text -> Text -> Text
 genInsert s1 s2 =
